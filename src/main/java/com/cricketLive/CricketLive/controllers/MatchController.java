1a71aa90c8f6b4cc161f069c8f02259a66c57c9b
Sajal <rayhansajal1996@gmail.com>
2024-02-24 14:23:15 +0600

    update

--- src/main/java/com/cricketLive/CricketLive/controllers/MatchController.java
+++ src/main/java/com/cricketLive/CricketLive/controllers/MatchController.java
@@ -4,6 +4,7 @@ import com.cricketLive.CricketLive.entities.Match;
 import com.cricketLive.CricketLive.services.MatchService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/match")
+@CrossOrigin("*")
 public class MatchController {
     private MatchService matchService;
 
